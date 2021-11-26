498d5b18cfc06f05190e32ad4dfee5cde8f8b788
david hurley <dhurls99@gmail.com>
2021-11-26 18:55:09 -0500

    limit how many nfts are generated

--- src/config.js
+++ src/config.js
@@ -25,7 +25,7 @@ const solanaMetadata = {
 // If you have selected Solana then the collection starts from 0 automatically
 const layerConfigurations = [
   {
-    growEditionSizeTo: 90,
+    growEditionSizeTo: 1200,
     layersOrder: [
       { name: "Background" },
       { name: "Card" },
@@ -34,7 +34,7 @@ const layerConfigurations = [
     ],
   },
   {
-    growEditionSizeTo: 100,
+    growEditionSizeTo: 1337,
     layersOrder: [{ name: "Background" }, { name: "Card" }, { name: "Joker" }],
   },
 ];
